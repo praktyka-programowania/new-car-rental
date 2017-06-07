9d9f61911cf1686d70e3ebf8446c3946aecf1113
Andrii Novitskyi <novitskiyandrew@gmail.com>
2017-06-08 00:44:55 +0200

    Some issues fixed

--- src/main/java/controllers/HomePageController.java
+++ src/main/java/controllers/HomePageController.java
@@ -53,43 +53,20 @@ public class HomePageController
             model.addAttribute(contact);
             return "contact";
         }
-
-        // Recipient's email ID needs to be mentioned.
         String to = "novitskiyandrew@gmail.com";
-
-        // Sender's email ID needs to be mentioned
         String from = contact.getEmail();
-
-        // Assuming you are sending email from localhost
         String host = "localhost";
-
-        // Get system properties
         Properties properties = System.getProperties();
-
-        // Setup mail server
         properties.setProperty("mail.smtp.host", host);
-
-        // Get the default Session object.
         Session session = Session.getDefaultInstance(properties);
 
         try
         {
-            // Create a default MimeMessage object.
             MimeMessage message = new MimeMessage(session);
-
-            // Set From: header field of the header.
             message.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-            // Set Subject: header field
             message.setSubject(contact.getFirstName() + " " + contact.getLastName() + " contact");
-
-            // Now set the actual message
             message.setText(contact.getMessage());
-
-            // Send message
             Transport.send(message);
         }
         catch (Exception mex)
